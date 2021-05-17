72d5175d87a224a85f374a16e28c72cb8589cea2
Chau Tran <cctran96@gmail.com>
2021-05-16 17:37:00 -0700

    commit tree 3fb18b82eb393996ea148106dd86ae92a10470c3

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map{|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(speakers)
+    arr = []
+    speakers.each.with_index(1){|speaker, index| arr << "Hello, #{speaker}! You'll be assigned to room #{index}!"}
+    arr
+end
+
+def printer(names)
+    batch_badge_creator(names).each{|message| puts message}
+    assign_rooms(names).each{|message| puts message}
+end
